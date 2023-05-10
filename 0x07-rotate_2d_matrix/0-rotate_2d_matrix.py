ab45e6c44b1f427468a3dbaea9c52743c46e156f
Olamide Joseph Williams <circleofwilliams@gmail.com>
2023-05-10 16:19:38 +0000

    Add function to rotate a 2D matrix

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""rotate_2d_matrix module
+"""
+
+
+def rotate_2d_matrix(matrix):
+    """A function to rotate a 2D matrix
+    """
+    matrix_len = len(matrix)
+    for i in range(k):
+        matrix_index = k - 1
+        for j in range(k):
+            matrix[i][j] = dup[matrix_index][i]
+            matrix_index -= 1
