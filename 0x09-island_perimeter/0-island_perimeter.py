73074d880d2ecf2a24e0cbde5094af7cd5be845d
Olamide Joseph Williams <circleofwilliams@gmail.com>
2023-05-24 21:42:53 +0000

    comments and indentation

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -1,16 +1,23 @@
+#!/usr/bin/python3
+
+'''
+A function to find the perimeter of an island
+'''
+
+
 def island_perimeter(grid):
-	'''
-		Finding the perimeter of the land area in an island.
-		arg: grid
-		return: the perimeter
-	'''
-	grid = grid[1 : -1]
-  for i in range(len(grid)):
-		grid[i] = grid[i][1 : -1]
-	grid = sorted(grid, reverse=True)
-  length = len(grid)
+    '''
+      Finding the perimeter of the land area in an island.
+      arg: grid
+      return: the perimeter
+    '''
+    grid = grid[1:-1]
+    for i in range(len(grid)):
+        grid[i] = grid[i][1:-1]
+    grid = sorted(grid, reverse=True)
+    length = len(grid)
 
-	g = [x for x in grid[0] if x == 1]
-	distance = len(g)
+    g = [x for x in grid[0] if x == 1]
+    distance = len(g)
 
-	return 2 * (length + distance)
+    return 2 * (length + distance)
