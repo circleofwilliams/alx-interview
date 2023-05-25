6b44875d4effed3acc8878d9392dfc7561cbd94c
Olamide Williams <104972300+circleofwilliams@users.noreply.github.com>
2023-05-25 11:23:03 +0100

    handling more test cases

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -6,18 +6,24 @@ A function to find the perimeter of an island
 
 
 def island_perimeter(grid):
-    '''
-      Finding the perimeter of the land area in an island.
-      arg: grid
-      return: the perimeter
-    '''
-    grid = grid[1:-1]
-    for i in range(len(grid)):
-        grid[i] = grid[i][1:-1]
-    grid = sorted(grid, reverse=True)
-    length = len(grid)
+    if 1 in grid[0] or 1 in grid[-1]:
+        return None
 
-    g = [x for x in grid[0] if x == 1]
-    distance = len(g)
-
-    return 2 * (length + distance)
+    grid_dup = []
+    for row in grid:
+        if 1 in row:
+            grid_dup.append(row)
+            
+    for i in range(len(grid_dup)):
+        grid_dup[i] = grid_dup[i][1:-1]
+            
+    print(grid_dup)
+    
+    if len(grid_dup) != 0:
+        grid_dup = sorted(grid_dup, reverse=True)
+        length = len(grid_dup)
+        g = [x for x in grid_dup[0] if x == 1]
+        distance = len(g)
+        return 2 * (length + distance)
+        
+    else: return 0
